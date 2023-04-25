7fdcc2dbbddf184034f2146b08aace31b2704642
JK Gonzalez <jkristian.gonzalez@gmail.com>
2023-04-25 17:18:31 -0500

    Update main.tf
    updated for container registry hardening

--- gke/main.tf
+++ gke/main.tf
@@ -1,4 +1,117 @@
-module "gke_monitoring" {
+ terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "mystical-button-380517"
+  region  = "us-central1"
+}
+
+module "gke_cluster" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/hardened_cluster"
+
+  project_id            = "mystical-button-380517"
+  name                  = "aptumcloud-k8s"
+  region                = "us-central1"
+  zones                 = ["us-central1-a", "us-central1-b", "us-central1-c"]
+  node_count            = 3
+  node_machine_type     = "n1-standard-2"
+  node_max_pods_per_node = 30
+
+  # Enable Shielded GKE Nodes
+  enable_shielded_nodes = true
+
+  # Configure the GKE cluster to use a hardened node image.
+  # Hardened images include CIS benchmark configurations and additional security features.
+  node_image_type = "COS_CONTAINERD"
+
+  # Binary Authorization
+  enable_binary_authorization = true
+
+  # Configure the GKE cluster to use private nodes and private endpoint.
+  enable_private_nodes   = true
+  master_authorized_networks_config = [
+    {
+      cidr_block = "10.0.0.0/8" # Allow only internal IP ranges
+    }
+  ]
+  private_cluster_config = {
+    enable_private_endpoint = true
+    master_ipv4_cidr_block = "172.16.0.0/28"
+    private_endpoint_dns_zone = "aptumcloud.com"
+  }
+
+  # Configure the GKE cluster to use workload identity and IAM roles for service accounts.
+  # This restricts permissions to the minimum necessary for each workload.
+  enable_workload_identity = true
+
+  # Configure the GKE cluster to use VPC-native networking.
+  enable_network_policy = true
+  network_policy_config = {
+    enabled = true
+    provider = "CALICO"
+  }
+
+  # Configure additional security features.
+  enable_pod_security_policy = true
+  enable_kubernetes_alpha = true
+
+  # Configure the GKE cluster to use RBAC and enforce strict PodSecurityPolicies.
+  enable_rbac = true
+  pod_security_policy = {
+    enabled = true
+    policy_types = ["PodSecurityPolicy"]
+  }
+
+  # Configure the GKE cluster to use a custom logging and monitoring solution.
+  # For example, you can use Prometheus, Fluentd, and Grafana to collect and analyze logs and metrics.
+  # You can also deploy Falco to monitor for abnormal behavior in your cluster.
+  logging_service = "none"
+  monitoring_service = "none"
+
+  # Configure the GKE cluster to use a custom container registry, such as Harbor.
+  add_ons_config = {
+    http_load_balancing = {
+      disabled = true
+    }
+    istio_config = {
+      disabled = true
+    }
+    kubernetes_dashboard = {
+      disabled = true
+    }
+    network_policy_config = {
+      disabled = true
+    }
+  }
+  
+  
+  # This example uses Google Container Registry, but you can use any other Docker registry.
+  master_auth_username = "_json_key"
+  master_auth_password = "${file("/path/to/docker-registry/key.json")}"
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/devstorage.full_control"
+    ]
+  }
+}
+
+  
+  
+  # Create a Kubernetes namespace for the logging and monitoring
+resource "kubernetes_namespace" "logging
+  
+  
+   
+ #Begin Cluster Bindings
+  
+  module "gke_monitoring" {
   source  = "terraform-google-modules/kubernetes-engine/google//modules/prometheus"
   version = "~> 16.0.0"
 
@@ -40,23 +153,7 @@ resource "google_organization_iam_binding" "gke_cluster_monitoring_binding" {
     "serviceAccount:my-gcp-service-account@my-gcp-project.iam.gserviceaccount.com",
   ]
 }
-
-resource "google_container_cluster" "main_cluster" {
-  name               = "main-cluster"
-  location           = "us-central1"
-  initial_node_count = 3
-
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-}
-    
-    
+  
 resource "google_container_cluster_oidc_authenticator_config" "oidc_auth_config" {
   count = var.authentication_method == "oidc" ? 1 : 0
 
@@ -81,6 +178,54 @@ resource "google_container_cluster_saml_authenticator_config" "saml_auth_config"
   }
 }
 
+  #This is the GCR Bucket which contains the encryption settings including the GC keyring for the registry
+
+resource "google_kms_key_ring" "gcr_key_ring" {
+  name     = "gcr-keyring"
+  location = "us-central1"
+}
+
+resource "google_kms_crypto_key" "gcr_crypto_key" {
+  name     = "gcr-key"
+  key_ring = google_kms_key_ring.gcr_key_ring.self_link
+}
+
+resource "google_storage_bucket" "gcr_bucket" {
+  name     = "aptumcloud-k8s-docker-registry"
+  location = "us-central1"
+
+  storage_class = "STANDARD"
+
+  versioning {
+    enabled = true
+  }
+
+  # Configure the GCR bucket to use customer-managed encryption keys
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.gcr_crypto_key.self_link
+  }
+}
+
+
+resource "google_service_account" "gcr_service_account" {
+  account_id   = "aptumcloud-k8s-docker-registry"
+  display_name = "Docker Registry Service Account"
+}
+
+resource "google_storage_bucket_iam_binding" "gcr_bucket_binding" {
+  bucket = google_storage_bucket.gcr_bucket.name
+  role   = "roles/storage.objectAdmin"
+
+  members = [
+    "serviceAccount:${google_service_account.gcr_service_account.email}"
+  ]
+}
+
+resource "google_service_account_key" "gcr_service_account_key" {
+  service_account_id = google_service_account.gcr_service_account.name
+}
+    
+#This is the main cluster buildout
     
  resource "google_container_cluster" "main_cluster" {
   name               = "main-cluster"
@@ -163,4 +308,21 @@ resource "google_storage_bucket_iam_member" "bucket_rbac_access" {
 # Members only provided in the email list have access to the monitoring bucket     
 # Adjust the email address(es) below to match the desired GCP account.
 #      member = "serviceAccount:my-gcp-service-account@my-gcp-project.iam.gserviceaccount.com"
+
+
+
+    # Configure the GKE cluster to use a custom Docker registry.
+  # This example uses Google Container Registry, but you can use any other Docker registry.
+  master_auth_username = "_json_key"
+  master_auth_password = "${base64decode(google_service_account_key.gcr_service_account_key.private_key)}"
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/devstorage.full_control"
+    ]
+  }
+}
+
+
       
