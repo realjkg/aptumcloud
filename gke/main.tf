bfc03b728d062efc4c90646353b9bf9787395caa
JK Gonzalez <jkristian.gonzalez@gmail.com>
2023-04-25 16:15:49 -0500

    Update main.tf
    committed saml and oidc changes for mfa.

--- gke/main.tf
+++ gke/main.tf
@@ -40,3 +40,42 @@ resource "google_organization_iam_binding" "gke_cluster_monitoring_binding" {
   ]
 }
 
+    resource "google_container_cluster" "main_cluster" {
+  name               = "main-cluster"
+  location           = "us-central1"
+  initial_node_count = 3
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_cluster_oidc_authenticator_config" "oidc_auth_config" {
+  count = var.authentication_method == "oidc" ? 1 : 0
+
+  cluster_name = google_container_cluster.main_cluster.name
+  location      = google_container_cluster.main_cluster.location
+
+  oidc_config {
+    issuer_url    = var.oidc_issuer_url
+    client_id     = var.oidc_client_id
+    k8s_api_audiences = [var.oidc_api_audience]
+  }
+}
+
+resource "google_container_cluster_saml_authenticator_config" "saml_auth_config" {
+  count = var.authentication_method == "saml" ? 1 : 0
+
+  cluster_name = google_container_cluster.main_cluster.name
+  location      = google_container_cluster.main_cluster.location
+
+  saml_config {
+    idp_metadata_url = var.saml_idp_metadata_url
+  }
+}
+
